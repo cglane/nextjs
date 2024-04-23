fda9171c164cc8fc0812e875f15d97e558d1bb1c
Charles Lane <charleslane23@gmail.com>
2024-04-23 09:58:54 -0400

    add splash block

--- app/progress/layout.tsx
+++ app/progress/layout.tsx
@@ -3,9 +3,15 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     
-    <div className="md:overflow-hidden">
+    <div className=" md:overflow-hidden">
         <HeadNav/>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        <hr  className=""></hr>
+
+        <div className='flex flex-col'>
+          <div  className='md:overflow-hidden bg-blue-dark h-60'></div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+
     </div>
   );
 }
--- app/ui/progress/progress.tsx
+++ app/ui/progress/progress.tsx
@@ -1,5 +1,5 @@
 export default function Progress() {
     return (
-        <h3 className="text-white rounded-md px-3 py-2 text-base font-medium">Progress..</h3>
+        <h3 className="text-white rounded-md px-3 py-2 text-base font-medium">[Progress..]</h3>
     )
 }
