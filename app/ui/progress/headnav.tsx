060da6cfa85076e0fb40c1a8a027358741020533
Charles Lane <charleslane23@gmail.com>
2024-04-23 09:49:21 -0400

    add placeholder for progress

--- app/ui/progress/headnav.tsx
+++ app/ui/progress/headnav.tsx
@@ -3,30 +3,19 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-
+import Progress from '@/app/ui/progress/progress'
 export default function HeadNav() {
   return (
-<nav className="bg-gray-800 base-color">
+<nav className="bg-blue-dark">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
         {/* <!-- Mobile menu button--> */}
         <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
           <span className="absolute -inset-0.5"></span>
-          {/* <span className="sr-only">Open main menu</span> */}
-          {/* <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          --> */}
           <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          {/* <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
           <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
@@ -35,31 +24,26 @@ export default function HeadNav() {
       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div className="flex flex-shrink-0 items-center">
         <Image
-        src="https://staging-cdn.casestatus.com/img/cs-logo-dark-03ae79696f1a088c15d686ac4b492506.svg"
+        src="https://staging-cdn.casestatus.com/img/cs-logo-mark-white-67379bc59ef8d31051ce0ebd1da4ddc3.svg"
         width={120}
         height={100}
         className="h-8 w-auto"
         alt="Case Status Icon"
   />
-          {/* <Image  width={100}height={100} className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/> */}
+      {/* https://tailwindcss.com/docs/font-size */}
+      <span className="text-white rounded-md px-3 py-2 text-lg font-bold">case status</span>
         </div>
 
         <div className="hidden sm:ml-64 sm:block">
-          <div className="flex space-x-4">
-            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-            {/* <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Case Status</a>             */}
-            <h3 className="text-white rounded-md px-3 py-2 text-sm font-medium">Progress..</h3>
+          <div className="flex space-x-4">            
+          <Progress/>
           </div>
         </div>
       </div>
       <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+        <button type="button" className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
           <span className="absolute -inset-1.5"></span>
-          {/* <span className="sr-only">View notifications</span> */}
-          <span >Finch Law Firm</span>
-          {/* <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-          </svg> */}
+          <h3 className="text-white rounded-md px-3 py-8 text-sm font-semibold">Finch Law Firm</h3>
         </button>
         
       </div>
