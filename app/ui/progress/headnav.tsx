c3e6621d5c91d31612b507c208f22cab7c50cc8d
Charles Lane <charleslane23@gmail.com>
2024-04-23 10:38:28 -0400

    nav flex formatting

--- app/ui/progress/headnav.tsx
+++ app/ui/progress/headnav.tsx
@@ -1,56 +1,42 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+
 import Image from 'next/image';
 import Progress from '@/app/ui/progress/progress'
 export default function HeadNav() {
   return (
-<nav className="bg-blue-dark">
-  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div className="relative flex h-16 items-center justify-between">
-      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        {/* <!-- Mobile menu button--> */}
-        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="absolute -inset-0.5"></span>
-          <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
-        <Image
-        src="https://staging-cdn.casestatus.com/img/cs-logo-mark-white-67379bc59ef8d31051ce0ebd1da4ddc3.svg"
-        width={120}
-        height={100}
-        className="h-8 w-auto"
-        alt="Case Status Icon"
-  />
-      {/* https://tailwindcss.com/docs/font-size */}
-      <span className="text-white rounded-md px-3 py-2 text-lg font-bold">case status</span>
-        </div>
+    <nav className="bg-blue-dark">
+      <div className="mx-auto max-w-1xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <Image
+                src="https://staging-cdn.casestatus.com/img/cs-logo-mark-white-67379bc59ef8d31051ce0ebd1da4ddc3.svg"
+                width={120}
+                height={100}
+                className="h-8 w-auto"
+                alt="Case Status Icon"
+              />
+              {/* https://tailwindcss.com/docs/font-size */}
+              <span className="text-white rounded-md px-3 py-2 text-lg font-bold">case status</span>
+            </div>
+          </div>   
+          <div>
+          <div className="hidden sm:ml-4 sm:block">
+              <div className="space-x-4">
+                <Progress />
+              </div>
+            </div>
+          </div>      
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button type="button" className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span className="absolute -inset-1.5"></span>
+              <h3 className="text-white rounded-md px-3 py-8 text-sm font-semibold">Finch Law Firm</h3>
+            </button>
 
-        <div className="hidden sm:ml-64 sm:block">
-          <div className="flex space-x-4">            
-          <Progress/>
           </div>
         </div>
       </div>
-      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-          <span className="absolute -inset-1.5"></span>
-          <h3 className="text-white rounded-md px-3 py-8 text-sm font-semibold">Finch Law Firm</h3>
-        </button>
-        
-      </div>
-    </div>
-  </div>
 
-</nav>
+    </nav>
 
   );
 }
