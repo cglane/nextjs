b1c44edd44efdc49b7e4b57210f6d63f4655a5d8
Charles Lane <charleslane23@gmail.com>
2024-04-24 16:35:18 -0400

    looking good

--- app/ui/progress/action-card.tsx
+++ app/ui/progress/action-card.tsx
@@ -27,11 +27,11 @@ export function ActionCard({
   
     return (
       <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-        <div className="md:flex p-4">
-          <CheckIcon className="w-8 h-8 text-green-400 bg-green-50 hidden md:block " />
-          <div className='md:w-9/12 w-full md:px-4'>
-            <h3 className="text-extrabold font-semibold w-full md:py-2">{title}</h3>            
-            <p className="my-2 text-xs text-gray-600 w-full md:py-2">{description}</p>
+        <div className="md:flex md:p-4">
+          <CheckIcon className="w-5 h-5 text-green-400 bg-green-50 hidden md:block " />
+          <div className='md:w-9/12 w-full md:px-2 text-center md:text-left'>
+            <h3 className="text-extrabold font-semibold w-full">{title}</h3>            
+            <p className="mb-4 text-xs text-gray-600 w-full md:py-1">{description}</p>
             <Link
             href="/login"
             className="items-center self-start rounded-lg px-2 py-2 text-xs md:text-sm outline outline-1 outline-offset-0 outline-gray-300"
@@ -41,7 +41,7 @@ export function ActionCard({
           </div>
           <div className='my-6 md:my-0 md:w-2/12 w-full justify-center'>
             <div>
-                <progress className="w-full " value={progress}/>
+                <progress className="w-full h-2 opacity-70" value={progress}/>
             </div>
             <div className="w-full text-center">
                 {
