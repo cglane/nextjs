90728dc7bec86047d778771782478da9c0470f0f
Charles Lane <charleslane23@gmail.com>
2024-05-07 13:06:13 -0400

    page setup

--- app/setup/practice_area/import_progress/page.tsx
+++ app/setup/practice_area/import_progress/page.tsx
@@ -1,30 +1,4 @@
 
-// "use client"
-// import { useStageStore } from "@/stores/stage";
-// import { useRouter } from 'next/navigation'
-// import { updateProgress } from "@/app/lib/actions";
-// export default  function Page() {      
-//   const router = useRouter()
-
-//   const {replace, stage} = useStageStore();
-  
-//   const handleClickListener = async () => {
-//     let nextPathName = "/setup/practice_area"
-//     await updateProgress('1', nextPathName)
-//     replace({"title": "Something Else", "index": 1, "subTitle": "Something"})
-//     router.push(nextPathName)
-//   };
-//     return (
-//     <main>
-//         <h1>Current Module:{stage.title}</h1>
-//           <button onClick={handleClickListener}         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-// >
-//             Click to Move on to the Next Module
-//       </button>  
-//     </main>
-//   );
-// }
-
 import useTranslation from 'next-translate/useTranslation'
 import LandingView from '@/app/ui/setup/practice_area/landing_view';
 import { MainWrapper } from '@/app/ui/setup/basic_elements';
